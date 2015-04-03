f5de14099355c1af720d9bff8be485a8d766d44d
MaziarA <maziara2@gmail.com>
2015-04-03 13:57:38 -0700

    check_UpMachines.sh script created and working.

---
+++ check_UpMachines.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#This script checks for the machines defined in the 'machines.conf' file and checks if the machine is up.
+#For each machine that finds to be up, it creates a file name according to the Label (defined in config file) 
+# in the 'UpMachines' subfolder.
+#This script better be run as an scheduled cron task
+
+ProjectPath=`dirname $0`
+LabelsPath="/UpMachines"
+while read line
+do
+  line=`echo $line | sed 's/^#.*//'`
+  if [ -n "$line" ]; 
+  then
+    label=`echo $line | awk -F " " '{print ($1)}'`;
+    address=`echo $line | awk -F " " '{print ($2)}'`;
+    port=`echo $line | awk -F " " '{print ($3)}'`;
+    wait_time=`echo $line | awk -F " " '{print ($4)}'`;
+
+    if nc -z -w $wait_time $address $port; then
+      touch $ProjectPath/$LabelsPath/$label;
+    else
+      if [ -e $ProjectPath/$LabelsPath/$label ]; then rm $ProjectPath/$LabelsPath/$label; fi
+    fi
+
+  fi
+done < $ProjectPath/machines.conf
